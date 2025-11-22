9f26034d70054c06d65254823557ae3e1a6023bc
윤재준 <fruitman200912@gmail.com>
2025-11-22 13:59:40 +0900

    refactor/재설정

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.species import Species
-from app.models.search_query import SearchQuery
-from app.models.region_biodiversity import RegionBiodiversity
-
-__all__ = ["Species", "SearchQuery", "RegionBiodiversity"]
